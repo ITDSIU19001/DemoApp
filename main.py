5b46371bd9b0b3d6b3fb639310c857f36dfe5696
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2023-06-28 03:46:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -497,19 +497,17 @@ if tabs == "Dashboard":
                 )
                 fig.update_layout(xaxis_title="Semeters",height=400, width=400)
                 st.plotly_chart(fig, use_container_width=True)
-    del raw_data1, df1, filtered_df1, mean_DiemHP, counts, bins, total_count, frequencies_percentage, grade_bins
-    if 'fig1' in locals():
-        del fig1
-    if 'common_elements' in locals():
-        del common_elements
-    if 'merged_array' in locals():
-        del merged_array
-    if 'list3' in locals():
-        del list3
-    if 'list3' in locals():
-        del dfac
-    del fig, new_df, new_dfa, new1_df, new1_dfa, course_data, course_data_dict
-    del options, valid_courses , list2, list1
+    variables_to_delete = [
+        'raw_data1', 'df1', 'filtered_df1', 'mean_DiemHP', 'counts', 'bins',
+        'total_count', 'frequencies_percentage', 'grade_bins', 'fig1',
+        'common_elements', 'merged_array', 'list3', 'dfac', 'fig', 'new_df',
+        'new_dfa', 'new1_df', 'new1_dfa', 'course_data', 'course_data_dict',
+        'options', 'valid_courses', 'list2', 'list1'
+    ]
+
+    for variable in variables_to_delete:
+        if variable in locals():
+            del locals()[variable]
 
 
 
