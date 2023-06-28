cfae43b7c93c86e2704f2a2810d1e120348937f4
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2023-06-28 14:32:20 +0700

    Update function.py

--- function.py
+++ function.py
@@ -110,7 +110,7 @@ def process_predict_data(raw_data):
 
     data = raw_data[["MaSV", "NHHK", "SoTCDat"]]
     data = (
-        data.groupby(["MaSV"])["SoTCDat"].mean().reset_index(name="Mean_Cre").round(2)
+        data.groupby(["MaSV"])["SoTCDat"].median().reset_index(name="Mean_Cre").round(2)
     )
 
     df = pd.merge(df, data, on="MaSV")
